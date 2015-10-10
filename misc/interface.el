877de994afaa30db38c945912e0ebdf0e5e67bd9
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-10 17:36:17 +0330

    Changing Alpha and default theme

--- misc/interface.el
+++ misc/interface.el
@@ -10,9 +10,9 @@
 (global-hl-line-mode)
 (global-visual-line-mode t)
 (winner-mode t)
-(custom-set-faces
- '(default ((t (:background "black" :foreground "grey"))))
- '(fringe ((t (:background "black")))))
+;; (custom-set-faces
+;;  '(default ((t (:background "black" :foreground "grey"))))
+;;  '(fringe ((t (:background "black")))))
 ;; (setq minibuffer-frame-alist
       ;; '((top . 1) (left . 3) (width . 80) (height . 2)))
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
@@ -21,16 +21,16 @@
 ;;; (set-frame-parameter nil 'fullscreen 'fullboth)
 ;;; (add-to-list 'default-frame-alist
 ;;; 	     'fullscreen 'fullboth)
-;; (set-frame-parameter (selected-frame)
-;; 		     'alpha '(95 80)
-;; 		     )
-;; (add-to-list 'default-frame-alist
-;; 	     '(alpha 95 80)
-;; 	     )
-;; (load-theme 'atom-dark t)
-;; (powerline-center-theme)
-;; (setq powerline-default-separator 'arrow)
-;; (load-theme 'airline-luna t)
+(set-frame-parameter (selected-frame)
+		     'alpha '(95 80)
+		     )
+(add-to-list 'default-frame-alist
+	     '(alpha 95 80)
+	     )
+(load-theme 'atom-dark t)
+(powerline-center-theme)
+(setq powerline-default-separator 'arrow)
+(load-theme 'airline-luna t)
 
 
 ;; ;; from here http://emacs-doctor.com/emacs-strip-tease.html
