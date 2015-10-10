f65e5903c3eec538f39e8e34e9b2205f5ea47401
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-10 18:36:48 +0330

    finally found a way to use multiple From: adresses
    I wanted to use multiple addresses to send email and it often was not
    related to which group I was in or which email I was replying, and it
    works perfectly. found it here:
    http://www.emacswiki.org/emacs/GnusTutorial#toc39

--- misc/messageInit.el
+++ misc/messageInit.el
@@ -12,5 +12,24 @@
 (add-hook 'message-mode-hook 'turn-on-orgstruct++)
 (add-hook 'message-mode-hook 'turn-on-orgtbl)
 
+;; From this guid on EmacsWiki
+;; http://www.emacswiki.org/emacs/GnusTutorial#toc39
+(setq message-alternative-emails
+      (regexp-opt my-email-addresses))
+;; Gnus from manipulation
+(setq gnus-from-selected-index 0)
+(defun gnus-loop-from ()
+  (interactive)
+  (setq gnus-article-current-point (point))
+  (goto-char (point-min))
+  (if (eq gnus-from-selected-index (length my-email-addresses))
+      (setq gnus-from-selected-index 0) nil)
+  (while (re-search-forward "^From:.*$" nil t)
+    (replace-match (concat "From: " user-full-name " <" (nth gnus-from-selected-index my-email-addresses) ">")))
+  (goto-char gnus-article-current-point)
+  (setq gnus-from-selected-index (+ gnus-from-selected-index 1)))
+
+(add-hook 'message-mode-hook '(lambda () (local-set-key (kbd "C-c f") 'gnus-loop-from))
+
 (provide 'messageInit)
 ;;; messageInit.el ends here
