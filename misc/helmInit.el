57c3c668635c8f225843662f843ec2e089b182cd
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-10 22:39:16 +0330

    Add an experimental function to use with sudo
    may not work exactly as intended and may get removed in future

--- misc/helmInit.el
+++ misc/helmInit.el
@@ -8,6 +8,13 @@
 (setq helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match    t)
 
+;; workaround to open files with sudo in helm-find-files
+(defadvice helm-find-files (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'helmInit)
 
 ;;; helmInit.el ends here
