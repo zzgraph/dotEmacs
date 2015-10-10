252508c4b19601da4e073ddf34ae779a0458280b
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-10 22:32:33 +0330

    polishing changes to messageInit
    remove some flucheck warnings

--- misc/messageInit.el
+++ misc/messageInit.el
@@ -1,7 +1,20 @@
-;;; Package --- messageInit.el
+;;; messageInit --- Cotumizations for message-mode
+;;
 ;;; Commentary:
+;;
+;; Using msmtp as sendmail program and make it read the From: header
+;; to choose account. also instruct message mode to support org-mode
+;; format. The function gnus-loop-from
+;;
+;;; Usage:
+;; Add this to your init file
+;;      (require 'messageInit)
+;;
+;;; This File Is Not Part of GNU Emacs
 ;;; Code:
-
+(require 'message)
+(require 'sendmail)
+(require 'google-contacts-message)
 (setq message-sendmail-f-is-evil t)
 (setq message-send-mail-function 'message-send-mail-with-sendmail)
 (setq message-sendmail-extra-arguments '("--read-envelope-from"))
@@ -18,18 +31,27 @@
       (regexp-opt my-email-addresses))
 ;; Gnus from manipulation
 (setq gnus-from-selected-index 0)
-(defun gnus-loop-from ()
+(defun message-mode-toggle-from ()
+  "Loop between addresses assigned to my-email-addresses!"
   (interactive)
   (setq gnus-article-current-point (point))
   (goto-char (point-min))
   (if (eq gnus-from-selected-index (length my-email-addresses))
       (setq gnus-from-selected-index 0) nil)
   (while (re-search-forward "^From:.*$" nil t)
-    (replace-match (concat "From: " user-full-name " <" (nth gnus-from-selected-index my-email-addresses) ">")))
+    (replace-match
+     (concat "From: "
+	     user-full-name
+	     " <"
+	     (nth gnus-from-selected-index my-email-addresses)
+	     ">")))
   (goto-char gnus-article-current-point)
   (setq gnus-from-selected-index (+ gnus-from-selected-index 1)))
 
-(add-hook 'message-mode-hook '(lambda () (local-set-key (kbd "C-c f") 'gnus-loop-from)))
+(add-hook 'message-mode-hook
+	  '(lambda ()
+	     (local-set-key (kbd "C-c f")
+			    'message-mode-toggle-from)))
 
 (provide 'messageInit)
 ;;; messageInit.el ends here
