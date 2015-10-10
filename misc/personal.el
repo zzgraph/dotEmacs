2540c8b37cf94a054b73459e45a9ec8a68eb7cb3
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-10 18:27:16 +0330

    Adding personal.el
    personal.el contains variables which contain personal information

---
+++ misc/personal.el
@@ -0,0 +1,11 @@
+;;; Package --- personal.el
+;;; Commentary:
+;; My personal information stays here
+;;; Code:
+(defvar my-email-addresses)
+(setq my-email-addresses '("zzgraph@gmail.com"
+			   "shahab.shahsavari@outlook.com"
+			   "shibi.shahsavari@gmail.com"))
+(provide 'personal)
+
+;;; personal.el ends here
