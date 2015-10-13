e24b3ea9b3e34866497ca628cb762a9304b35eda
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-13 19:09:57 +0330

    add mandatory comments to aoutocompleteInit

--- misc/autocompleteInit.el
+++ misc/autocompleteInit.el
@@ -1,5 +1,10 @@
+;;; autocompleteInit.el --- Configurations for ac
+;;; Commentary:
+;;; Code:
+
 (require 'auto-complete-config)
 (ac-config-default)
 (setq ac-show-menu-immediately-on-auto-complete t)
 
 (provide 'autocompleteInit)
+;;; autocompleteInit.el ends here
