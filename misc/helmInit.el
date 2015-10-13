64e300855b4938a137dadd704ab6929b9e1018d9
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-13 19:14:14 +0330

    Added helm descbinds and remove sudo find file
    the function to use tramp root local account to open unwritable files
    causes problems, commented out

--- misc/helmInit.el
+++ misc/helmInit.el
@@ -2,18 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 (require 'helm-config)
-
+(require 'helm-descbinds)
+(helm-descbinds-mode)
 (helm-mode 1)
 (setq helm-M-x-fuzzy-match t)
 (setq helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match    t)
 
 ;; workaround to open files with sudo in helm-find-files
-(defadvice helm-find-files (after find-file-sudo activate)
-  "Find file as root if necessary."
-  (unless (and buffer-file-name
-               (file-writable-p buffer-file-name))
-    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+;; (defadvice helm-find-files (after find-file-sudo activate)
+;;   "Find file as root if necessary."
+;;   (unless (and buffer-file-name
+;;                (file-writable-p buffer-file-name))
+;;     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 (provide 'helmInit)
 
