bb73ed6651124a7150dd71e046b284043aad801c
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-13 19:20:41 +0330

    Working Persian fontset
    with some advice from stack exchange in here http://stackoverflow.com/questions/33074370/how-can-i-use-a-different-ttf-fonts-for-certain-utf-8-characters-in-emacs?noredirect=1#comment53975001_33074370

---
+++ misc/customFacesInit.el
@@ -0,0 +1,14 @@
+;;; customeFacesInit.el --- My Custome fonts
+;;;
+;;; Commentary:
+;;; Code:
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-10"))
+(defun zzgraph/fix-fontset (&optional frame)
+  (set-fontset-font "fontset-default" '(#x10000 . #x1ffff) "Symbola-18")
+  (set-fontset-font "fontset-default" 'arabic "Roya-12"))
+(zzgraph/fix-fontset)
+(add-hook 'after-make-frame-functions 'zzgraph/fix-fontset)
+
+(provide 'customFacesInit)
+;;; customFacesInit.el ends here
+
