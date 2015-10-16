a06bc23a432abeb595c9efd4006ab8d1aa207f7b
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-16 16:13:38 +0330

    play with emms to check that

---
+++ misc/emmsInit.el
@@ -0,0 +1,10 @@
+;;; emmsInit.el --- Configurations for emms
+;;; Commentary:
+;;; Code:
+
+(require 'emms-setup)
+(emms-standard)
+(emms-default-players)
+
+(provide 'emmsInit)
+;;; emmsInit.el ends here
