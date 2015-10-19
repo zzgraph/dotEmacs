43c0609a5e879f57048dfa5bfa43cfcc6142c064
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-19 09:23:01 +0330

    Configurations for diminish

---
+++ misc/diminishInit.el
@@ -0,0 +1,15 @@
+;;; diminishInit.el --- List of minor modes to be diminished on startup
+;;; Commentary:
+;;; Code:
+(require 'diminish)
+
+(eval-after-load "flyspell" '(diminish 'flyspell-mode))
+(eval-after-load "flycheck" '(diminish 'flycheck-mode))
+(eval-after-load "company" '(diminish 'company-mode))
+(eval-after-load "helm" '(diminish 'helm-mode))
+(eval-after-load "yasnippet" '(diminish 'yas-minor-mode))
+(eval-after-load "simple" '(diminish 'visual-line-mode))
+(eval-after-load "undo-tree" '(diminish 'undo-tree-mode))
+
+(provide 'diminishInit)
+;;; diminishInit.el ends here
