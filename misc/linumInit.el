3d30005baabddbf77589cda2e84680ae1c92284f
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-10-22 11:03:56 +0330

    Let's don't use global-mode
    specially when you want to disable it everywhere, just use it in
    progn-mode will be fine, if it's needed anywhere elese you can addhook

--- misc/linumInit.el
+++ misc/linumInit.el
@@ -1,7 +1,7 @@
-;;; Package --- linumInit.el
+;;; linumInit.el --- Configuration to show line numbers
 ;;; Commentary:
 ;;; Code:
-(global-linum-mode t)
+;; (global-linum-mode t)
 ;; (setq linum-format (if (not window-system) "%4d " "%4d"))
 
 ;; Ensure linum-mode is disabled in certain major modes.
@@ -14,54 +14,61 @@
 ;; (defun linum-on ()
 ;;   (unless (or (minibufferp) (member major-mode linum-disabled-modes))
 ;;     (linum-mode 1)))
-;http://stackoverflow.com/questions/3875213/ \
-;turning-on-linum-mode-when-in-python-c-mode
-(defvar linum-mode-inhibit-modes-list)
-(setq linum-mode-inhibit-modes-list '(eshell-mode
-                                      shell-mode
-                                      dictionary-mode
-                                      erc-mode
-                                      browse-kill-ring-mode
-                                      etags-select-mode
-                                      dired-mode
-                                      help-mode
-                                      text-mode
-                                      fundamental-mode
-                                      jabber-roster-mode
-                                      jabber-chat-mode
-                                      inferior-scheme-mode
-                                      twittering-mode
-                                      compilation-mode
-                                      weibo-timeline-mode
-                                      woman-mode
-                                      Info-mode
-                                      calc-mode
-                                      calc-trail-mode
-                                      comint-mode
-                                      gnus-group-mode
-                                      inf-ruby-mode
-                                      gud-mode
-                                      org-mode
-                                      vc-git-log-edit-mode
-                                      log-edit-mode
-                                      term-mode
-                                      w3m-mode
-                                      speedbar-mode
-                                      gnus-summary-mode
-                                      gnus-article-mode
-                                      calendar-mode
-				      inferior-python-mode
-				      inferior-emacs-lisp-mode
-				      python-django-mode
-				      messages-buffer-mode
-				      cider-repl-mode
-				      Custom-mode))
-(defadvice linum-on (around linum-on-inhibit-for-modes)
-           "Stop the load of linum-mode for some major modes."
-           (unless (member major-mode linum-mode-inhibit-modes-list)
-             ad-do-it))
-(ad-activate 'linum-on)
-(require 'hlinum)
-(hlinum-activate)
+;; ;http://stackoverflow.com/questions/3875213/ \
+;; ;turning-on-linum-mode-when-in-python-c-mode
+;; (defvar linum-mode-inhibit-modes-list)
+;; (setq linum-mode-inhibit-modes-list '(eshell-mode
+;;                                       shell-mode
+;;                                       dictionary-mode
+;;                                       erc-mode
+;;                                       browse-kill-ring-mode
+;;                                       etags-select-mode
+;;                                       dired-mode
+;;                                       help-mode
+;;                                       text-mode
+;;                                       fundamental-mode
+;;                                       jabber-roster-mode
+;;                                       jabber-chat-mode
+;;                                       inferior-scheme-mode
+;;                                       twittering-mode
+;;                                       compilation-mode
+;;                                       weibo-timeline-mode
+;;                                       woman-mode
+;;                                       Info-mode
+;;                                       calc-mode
+;;                                       calc-trail-mode
+;;                                       comint-mode
+;;                                       gnus-group-mode
+;;                                       inf-ruby-mode
+;;                                       gud-mode
+;;                                       org-mode
+;;                                       vc-git-log-edit-mode
+;;                                       log-edit-mode
+;;                                       term-mode
+;;                                       w3m-mode
+;;                                       speedbar-mode
+;;                                       gnus-summary-mode
+;;                                       gnus-article-mode
+;;                                       calendar-mode
+;; 				      inferior-python-mode
+;; 				      inferior-emacs-lisp-mode
+;; 				      python-django-mode
+;; 				      messages-buffer-mode
+;; 				      cider-repl-mode
+;; 				      Custom-mode
+;; 				      gnus-browse-mode))
+;; (defadvice linum-on (around linum-on-inhibit-for-modes)
+;;            "Stop the load of linum-mode for some major modes."
+;;            (unless (member major-mode linum-mode-inhibit-modes-list)
+;;              ad-do-it))
+;; (ad-activate 'linum-on)
+
+;; Isn't it silly to first start a global mode and then inhibit it in
+;; almost "every other" modes? I think it's silly! let see how is this
+;; going to work, and let's use nlinum instead of linum
+(require 'nlinum)
+(add-hook 'prog-mode-hook 'nlinum-mode)
+;; (require 'hlinum)
+;; (hlinum-activate)
 
 (provide 'linumInit)
