67480052ba49c471341182ae69110e233072839f
Shahab Shahsavari Alavidjeh <shibi@fedora22.zzgraph>
2015-11-12 17:16:22 +0330

    initial commit for eldoc configuration

---
+++ misc/eldocInit.el
@@ -0,0 +1,13 @@
+;;; eldocInit --- Initialize eldoc for certain modes
+;;; Commentary:
+;;; Code:
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+
+(provide 'eldocInit)
+
+;;; eldocInit.el ends here
