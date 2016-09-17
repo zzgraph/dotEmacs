01239c323412a6f19c95309d1b752cd5683cea16
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2016-09-17 12:38:19 +0430

    new ivy configuration file

---
+++ misc/ivyInit.el
@@ -0,0 +1,11 @@
+;;; ivyInit.el --- Configurations for ivy
+;;; Commentary:
+;;; Code:
+
+(require 'ivy)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+
+
+(provide 'ivyInit)
+;;; ivyInit.el ends here
