ced8db16f501f099b2c6a885b947ad87540a1306
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-05-18 01:52:43 +0430

    Minor typo correction

--- misc/emmetInit.el
+++ misc/emmetInit.el
@@ -7,5 +7,5 @@
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 
-(provide 'emmmtInit)
+(provide 'emmetInit)
 ;;; emmetInit.el ends here
