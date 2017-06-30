664f485de522f0e8b3cc25be88bfe53a363f07d0
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-01 00:23:30 +0430

    Adding ace-window keybindings

--- misc/keyBindings.el
+++ misc/keyBindings.el
@@ -37,8 +37,12 @@
 ;; Ace-jump Keybinding
 ;; (global-set-key (kbd "s-j") 'ace-jump-mode)
 
+
 ;; Ace-window Keybinding
-;; (global-set-key (kbd "s-w") 'ace-window)
+(require 'ace-window)
+
+(global-set-key (kbd "M-p") 'ace-window)
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
 ;; Google Translator
 (global-set-key (kbd "C-c t") 'google-translate-smooth-translate)
