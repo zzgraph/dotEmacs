219f25b794c9bd010814a6f21419913a8b81e6c4
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-01 01:18:35 +0430

    Adding gitgutter to the bunch

---
+++ misc/gitGutter.el
@@ -0,0 +1,36 @@
+;;; gitGutter.el --- Configurations for gitgutter
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(require 'git-gutter)
+
+;; If you enable global minor mode
+(global-git-gutter-mode t)
+
+;; If you would like to use git-gutter.el and linum-mode
+(git-gutter:linum-setup)
+
+;; If you enable git-gutter-mode for some modes
+(add-hook 'ruby-mode-hook 'git-gutter-mode)
+
+(global-set-key (kbd "C-x C-g") 'git-gutter)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+
+;; Jump to next/previous hunk
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+
+;; Stage current hunk
+(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+
+;; Revert current hunk
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+
+;; Mark current hunk
+(global-set-key (kbd "C-x v SPC") #'git-gutter:mark-hunk)
+
+
+(provide 'gitGutter)
+;;; gitGutter.el ends here
