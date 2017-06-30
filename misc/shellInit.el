8656ecb52e00361483994692c76d6ba44b7f7a82
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-01 01:20:15 +0430

    Shell pop works but ...

--- misc/shellInit.el
+++ misc/shellInit.el
@@ -6,14 +6,14 @@
 
 (require 'shell-pop)
 
-(setq shell-pop-default-directory "/home/shibi")
-(setq shell-pop-shell-type "shell")
-(setq shell-pop-term-shell "/bin/bash")
-(setq shell-pop-universal-key "C-s-t")
+;; (setq shell-pop-default-directory "/home/shibi")
+(setq shell-pop-shell-type "eshell")
+;; (setq shell-pop-term-shell "/bin/bash")
+(setq shell-pop-universal-key [f12])
 (setq shell-pop-window-size 20)
 (setq shell-pop-full-span t)
-(setq shell-pop-window-position "bottom")
-(global-set-key [f12] 'shell-pop)
+;(setq shell-pop-window-position "bottom")
+;(global-set-key [f12] 'shell-pop)
 
 
 (provide 'shellInit)
