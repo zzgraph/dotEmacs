6e2bd37b6a7eec06538a7c4ab32cd7af6e40b057
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-01 01:20:58 +0430

    Adding tsx files to webmode

--- misc/webInit.el
+++ misc/webInit.el
@@ -15,7 +15,11 @@
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
 
 (define-key web-mode-map (kbd "C-'") 'company-web-html)
 (setq web-mode-engines-alist  '(
