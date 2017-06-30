10489c8d1d54cec6eef792e506dfd77d509179d1
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-06-30 20:31:10 +0430

    Adding JS2 to diminish does not seems to work

--- misc/diminishInit.el
+++ misc/diminishInit.el
@@ -1,10 +1,11 @@
 ;;; diminishInit.el --- List of minor modes to be diminished on startup
 ;;; Commentary:
 ;;; Code:
+
 (require 'diminish)
 
 (eval-after-load "flyspell" '(diminish 'flyspell-mode))
-(eval-after-load "flycheck" '(diminish 'flycheck-mode))
+;; (eval-after-load "flycheck" '(diminish 'flycheck-mode))
 (eval-after-load "company" '(diminish 'company-mode))
 (eval-after-load "helm" '(diminish 'helm-mode))
 (eval-after-load "yasnippet" '(diminish 'yas-minor-mode))
@@ -15,6 +16,10 @@
 (eval-after-load "paredit" '(diminish 'paredit-mode))
 (eval-after-load "eldoc" '(diminish 'eldoc-mode))
 (eval-after-load "editorconfig" '(diminish 'editorconfig-mode))
+(eval-after-load "skewer" '(diminish 'skewer-mode))
+(eval-after-load 'js2-mode '(diminish 'js2-mode "JS2"))
+(diminish 'auto-revert-mode)
+
 
 
 
