164c41a0860bfe75932da32818e8fae3447f35ee
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-02 21:58:42 +0430

    Using xterm-color instead of ansi-color to get 256 dolor in comint

--- misc/shellInit.el
+++ misc/shellInit.el
@@ -4,16 +4,27 @@
 
 ;;; Code:
 
-(require 'shell-pop)
+;; comint install
 
-;; (setq shell-pop-default-directory "/home/shibi")
-(setq shell-pop-shell-type "eshell")
-;; (setq shell-pop-term-shell "/bin/bash")
-(setq shell-pop-universal-key [f12])
-(setq shell-pop-window-size 20)
-(setq shell-pop-full-span t)
-;(setq shell-pop-window-position "bottom")
-;(global-set-key [f12] 'shell-pop)
+(require 'bash-completion)
+(bash-completion-setup)
+
+(require 'xterm-color)
+(progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+       (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions)))
+
+
+(defun zzgraph/init-eshell-xterm-color ()
+  "Initialize xterm coloring for eshell."
+
+  (setq-local xterm-color-preserve-properties t)
+  (make-local-variable 'eshell-preoutput-filter-functions)
+  (add-hook 'eshell-preoutput-filter-functions 'xterm-color-filter)
+  (setq-local eshell-output-filter-functions
+              (remove 'eshell-handle-ansi-color
+		      eshell-output-filter-functions)))
+
+(add-hook 'eshell-mode-hook 'zzgraph/init-eshell-xterm-color)
 
 
 (provide 'shellInit)
