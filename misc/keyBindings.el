a9f19f9866d9bb7d1612c847fb7bcd60aa07bb58
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-02 22:06:00 +0430

    adding neotree, gitgutter, ace-jump to key-bindings

--- misc/keyBindings.el
+++ misc/keyBindings.el
@@ -4,25 +4,24 @@
 ;;; Code:
 ;; Browse URL Key Bindings
 ;; To Browse URLs on the text fles in Firefox
-(global-set-key "\C-c\C-z." 'browse-url-at-point)
-(global-set-key "\C-c\C-zb" 'browse-url-of-buffer)
-(global-set-key "\C-c\C-zr" 'browse-url-of-region)
-(global-set-key "\C-c\C-zu" 'browse-url)
-(global-set-key "\C-c\C-zv" 'browse-url-of-file)
+(global-set-key (kbd "C-c C-z .") 'browse-url-at-point)
+(global-set-key (kbd "C-c C-z b") 'browse-url-of-buffer)
+(global-set-key (kbd "C-c C-z r") 'browse-url-of-region)
+(global-set-key (kbd "C-c C-z u") 'browse-url)
+(global-set-key (kbd "C-c C-z v") 'browse-url-of-file)
 (add-hook 'dired-mode-hook
 	  (lambda ()
-	    (local-set-key "\C-c\C-zf" 'browse-url-of-dired-file)))
+	    (local-set-key (kbd "C-c C-z f") 'browse-url-of-dired-file)))
 
 ;; Keybinding for unfill paragraph function
-(define-key global-map "\M-Q" 'unfill-paragraph)
+(define-key global-map (kbd "M-Q") 'unfill-paragraph)
 
 ;; Orgmode Key Bindings
 ;; From Org Manual
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
-
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
 ;; smex Keybindings
 ;; (global-set-key (kbd "M-x") 'smex)
 ;; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
@@ -32,10 +31,17 @@
 
 
 ;; when we have windmove why we need this?
-(global-set-key (kbd "C-M-z") 'switch-window)
+;; (global-set-key (kbd "C-M-z") 'switch-window)
+
+
+;; shell-pop
+(global-set-key (kbd "<f12>") 'shell-pop)
+
+;; neotree
+(global-set-key (kbd "<f8>") 'neotree-toggle)
 
 ;; Ace-jump Keybinding
-;; (global-set-key (kbd "s-j") 'ace-jump-mode)
+(global-set-key (kbd "C-x j") 'ace-jump-mode)
 
 
 ;; Ace-window Keybinding
@@ -51,6 +57,19 @@
 ;; gnus message-mode instead of emacs mail-mode
 (global-unset-key (kbd "C-x m"))
 (global-set-key (kbd "C-x m") 'message-mail)
+
+;; keybindings for gitgutter-mode
+(global-set-key (kbd "C-x C-g") 'git-gutter)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+;; Jump to next/previous hunk
+(global-set-key (kbd "C-x v p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x v n") 'git-gutter:next-hunk)
+;; Stage current hunk
+(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+;; Revert current hunk
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+;; Mark current hunk
+(global-set-key (kbd "C-x v SPC") #'git-gutter:mark-hunk)
 
 ;; Helm Bindings
 ;; (global-set-key (kbd "M-x") 'helm-M-x)
@@ -96,7 +115,7 @@
 
 
 ;; python-django-mode key-binding
-(global-set-key (kbd "C-x j") 'python-django-open-project)
+;; (global-set-key (kbd "C-x j") 'python-django-open-project)
 
 
 ;; Enable ibuffer instead of Buffer-list
