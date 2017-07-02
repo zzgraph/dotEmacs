1ab3e0962240afbaafb520ce13ff392b54ea9119
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-02 21:59:34 +0430

    more condifuration for js2 mode and using prettifying symbols forfun

--- misc/jsInit.el
+++ misc/jsInit.el
@@ -4,8 +4,39 @@
 ;; script inferior mode
 
 ;;; Code:
+
+(require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+(setq js-basic-indent 2)
+  (setq-default js2-basic-indent 2
+                js2-basic-offset 2
+                js2-auto-indent-p t
+                js2-cleanup-whitespace t
+                js2-enter-indents-newline t
+                js2-indent-on-enter-key t
+		js2-strict-missing-semi-warning t
+                js2-global-externs (list "window" "module" "require" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "jQuery" "$"))
+
+;; Initiating tern mode when js2-mode starts
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+;; Just prettify some symbols for fun
+(add-hook 'js2-mode-hook
+	  (lambda ()
+	    (push '("function" . ?ƒ) prettify-symbols-alist)
+	    (push '("!==" . ?≠) prettify-symbols-alist)
+	    (push '(">=" . ?≥) prettify-symbols-alist)
+	    (push '("<=" . ?≤) prettify-symbols-alist)
+	    (push '("undefined" . ?∅) prettify-symbols-alist)
+	    (push '("===" . ?≡) prettify-symbols-alist)
+	    (push '("==" . ?≈) prettify-symbols-alist)
+	    (push '("=>" . ?→) prettify-symbols-alist)
+	    (push '("" . ?ƒ) prettify-symbols-alist)
+	    ))
+
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 
 (provide 'jsInit)
