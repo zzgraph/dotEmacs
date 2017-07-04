8325c9c45ede0243b96f3b5515323893011da4b4
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-05 00:23:55 +0430

    Reduce the size for symbola fonts

--- misc/customFacesInit.el
+++ misc/customFacesInit.el
@@ -3,8 +3,9 @@
 ;;; Commentary:
 ;;; Code:
 (add-to-list 'default-frame-alist '(font . "Vazir Code-12"))
-(defun zzgraph/fix-fontset (&optional frame)
-  (set-fontset-font "fontset-default" '(#x10000 . #x1ffff) "Symbola-18")
+(defun zzgraph/fix-fontset ()
+  "Adding Symbola font to fontse to show emoji utf-8 symbols."
+  (set-fontset-font "fontset-default" '(#x10000 . #x1ffff) "Symbola-13")
 ;;  (set-fontset-font "fontset-default" 'arabic "DejaVu Sans")
   )
 (zzgraph/fix-fontset)
