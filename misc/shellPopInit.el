0e6d3b9f241625a49ca524616299ea1f5c443c0b
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-04 14:06:52 +0430

    Working settings for shell pop, only works with custom-set-variable

--- misc/shellPopInit.el
+++ misc/shellPopInit.el
@@ -6,16 +6,18 @@
 
 (require 'shell-pop)
 
-;; (setq shell-pop-default-directory "/home/shibi")
-;; (setq shell-pop-shell-type  (quote ("eshell" "*eshell*"
-;; 				    (lambda nil (eshell)))))
-;; ;; (setq shell-pop-term-shell "/bin/bash")
-;; (setq shell-pop-universal-key "<f12>")
-;; (setq shell-pop-window-size 20)
-;; (setq shell-pop-full-span t)
-;; (setq shell-pop-window-position "bottom")
-;; (setq shell-pop-full-span t)
-;(global-set-key [f12] 'shell-pop)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(shell-pop-default-directory nil)
+ '(shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
+ '(shell-pop-term-shell "/bin/bash")
+ '(shell-pop-universal-key "<f12>")
+ '(shell-pop-window-size 20)
+ '(shell-pop-full-span t)
+ '(shell-pop-window-position "bottom"))
 
 (provide 'shellPopInit)
-;;; shellInit.el ends here
+;;; shellPopInit.el ends here
