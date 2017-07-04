2f08659f32acd3eebadef12b42f900873bb53232
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-04 14:38:54 +0430

    Adding comments to markDownInit and make it more comprehensive

--- misc/markDownInit.el
+++ misc/markDownInit.el
@@ -6,17 +6,37 @@
 
 (require 'markdown-mode)
 
+;; Associating .md and .markdown files with markdown major mode
+
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+;; special mode for README.md files which almost always use github
+;; flavoured markdown or gfm
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+;; loading emmetmode inside markdown files for arbitrary html files.
 (add-hook 'markdown-mode-hook 'emmet-mode)
 
+;; Using pandoc as markdown command
 (setq markdown-command "pandoc -f markdown -t html5")
 
+;; TODO find a way to enable font-lock highlighting for each
+;; programming language in code blocks
+
+;; TODO see if electric-pair-mode for insert pairs of #, *, **, and
+;; see if it's necessary.
+
+;; Use a temporary buffer with appropriate major mode to edit
+;; code-blocks
 (eval-after-load 'markdown-mode
   '(define-key markdown-mode-map (kbd "C-c '") 'markdown-edit-indirect))
 
+;; Snippet from emacswiki
+;; https://www.emacswiki.org/emacs/MarkdownMode#toc6 this helps to
+;; change exported html files for previewing in brwser use standard
+;; html5 doctype headers instead of xhtml and let UTF8 content show
+;; correctly in preview
 (eval-after-load "markdown-mode"
   '(defalias 'markdown-add-xhtml-header-and-footer 'zzgraph/markdown-add-xhtml-header-and-footer))
 
