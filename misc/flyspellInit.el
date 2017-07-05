771e5b247888bd69f3ae76f9d17774ef3a34b744
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-06 00:40:03 +0430

    binding `C-;` to ivy flyspell

--- misc/flyspellInit.el
+++ misc/flyspellInit.el
@@ -15,25 +15,30 @@
 	    (lambda ()
 	      (flyspell-prog-mode))))
 
-;; Changing local dictionaries
-;; http://unix.stackexchange.com/questions/86554/make-hunspell-work-with-emacs-and-german-language?answertab=active#tab-top
-(add-to-list 'ispell-local-dictionary-alist '("fa_IR"
-                                              "[[:alpha:]]"
-                                              "[^[:alpha:]]"
-                                              "[']"
-                                              t
-                                              ("-d" "fa_IR"); Dictionary file name
-                                              nil
-                                              utf-8))
+(require 'flyspell-correct-ivy)
+(define-key flyspell-mode-map (kbd "C-;")
+  'flyspell-correct-previous-word-generic)
 
-(add-to-list 'ispell-local-dictionary-alist '("en_US"
-                                              "[[:alpha:]]"
-                                              "[^[:alpha:]]"
-                                              "[']"
-                                              t
-                                              ("-d" "en_US")
-                                              nil
-                                              utf-8))
+;; Changing local dictionaries
+;; https://unix.stackexchange.com/a/112190
+(add-to-list
+ 'ispell-local-dictionary-alist '("fa_IR"
+				  "[[:alpha:]]"
+				  "[^[:alpha:]]"
+				  "[']"
+				  t
+				  ("-d" "fa_IR"); Dictionary file name
+				  utf-8))
+
+(add-to-list
+ 'ispell-local-dictionary-alist '("en_US"
+				  "[[:alpha:]]"
+				  "[^[:alpha:]]"
+				  "[']"
+				  t
+				  ("-d" "en_US")
+				  nil
+				  utf-8))
 
 (setq ispell-program-name "hunspell"          ; Use hunspell to correct mistakes
       ispell-dictionary   "en_US") ; Default dictionary to use
