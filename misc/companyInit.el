828d8e8591eff6a8284be227d2c02300eef7860d
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-06 23:14:03 +0430

    Working company but it no work as expected.

--- misc/companyInit.el
+++ misc/companyInit.el
@@ -3,46 +3,69 @@
 ;;; Code:
 (require 'company)
 (require 'slime-company)
-(require 'company-web-html)                          ; load company mode html backend
+(require 'company-web-html)        ; load company mode html backend
 ;; and/or
-(require 'company-web-jade)                          ; load company mode jade backend
-(require 'company-web-slim)                          ; load company mode slim backend
+(require 'company-web-jade)        ; load company mode jade backend
+(require 'company-web-slim)        ; load company mode slim backend
+
+;; (require 'readline-complete)
 
 ;; you may key bind, for example for web-mode:
-
-
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; (push 'slime-company company-backends)
-
-;; Adding shell scripting completion and doc string support to company
-(add-to-list 'company-backends 'company-shell)
-
-;; Adding tern to company backends for javascript files
-(add-to-list 'company-backends 'company-tern)
-
-(push 'company-readline company-backends)
-(add-hook 'rlc-no-readline-hook (lambda () (company-mode -1)))
-
-(add-to-list 'company-backends 'company-restclient)
-
-
-(company-quickhelp-mode 1)
 
 (eval-after-load 'company
-  '(define-key company-active-map (kbd "M-s-h") #'company-quickhelp-manual-begin))
+  '(progn
 
-(setq company-tooltip-limit 20)                      ; bigger popup window
-(setq company-tooltip-align-annotations 't)          ; align annotations to the right tooltip border
-(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
-(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
-(global-set-key (kbd "C-c /") 'company-files)        ; Force complete file names on "C-c /" key
+     ;; Adding shell scripting completion and doc string support to company
+     ;; (add-to-list 'company-backends 'company-shell)
+
+     ;; Adding tern to company backends for javascript files
+     (add-to-list 'company-backends 'company-tern)
+
+     (company-quickhelp-mode 1)
+
+     ;; (add-to-list 'company-backends 'company-readline)
+     ;; (add-hook 'rlc-no-readline-hook (lambda () (company-mode -1)))
+
+     (add-to-list 'company-backends 'company-restclient)
+
+
+     (setq
+      ;; bigger popup window
+      company-tooltip-limit 20
+
+      ;; align annotations to the right tooltip border
+      company-tooltip-align-annotations 't
+
+      ;; decrease delay before autocompletion popup shows
+      company-idle-delay 0.2
+
+      ;; press M-number to choose candidate from popup
+      company-show-numbers t
+
+      company-require-match nil
+
+      ;; start autocompletion only after typing
+      company-begin-commands '(self-insert-command)
+
+      ;; don't downcase abbreviations and be case sensitive
+      company-dabbrev-downcase nil
+      company-dabbrev-ignore-case nil
+      )
+
+     ;; NOT to load company-mode for certain major modes.
+     ;; https://github.com/redguardtoo/emacs.d/blob/master/lisp/init-company.el#L50
+     (setq company-global-modes
+           '(not
+             eshell-mode comint-mode shell-mode erc-mode gud-mode rcirc-mode
+	     minibuffer-inactive-mode))))
 
 
 
 
 ;; Source: https://gist.github.com/fletch/cefeb0ebe01552081d10
-;; http://emacs.stackexchange.com/questions/10431/get-company-to-show-suggestions-for-yasnippet-names
+;; https://emacs.stackexchange.com/a/10520
 ;; Add yasnippet support for all company backends
 ;; https://github.com/syl20bnr/spacemacs/pull/179
 ;; (defvar company-mode/enable-yas t
@@ -50,24 +73,32 @@
 
 ;; (defun company-mode/backend-with-yas (backend)
 ;;   "Add yasnippet support for all company BACKENDs."
-;;   (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+;;   (if (or (not company-mode/enable-yas) (and (listp backend)
+;;(member 'company-yasnippet backend)))
 ;;       backend
 ;;     (append (if (consp backend) backend (list backend))
 ;;             '(:with company-yasnippet))))
 
-;; (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+;; (setq company-backends
+;; (mapcar #'company-mode/backend-with-yas company-backends))
 
 
 ;; helm-company choose from company completions with C-:
 ;; (with-eval-after-load 'company
 ;; (define-key company-mode-map (kbd "C-:") 'helm-company)
 ;; (define-key company-active-map (kbd "C-:") 'helm-company))
+
+
 ;; (require 'color)
 ;; (let ((bg (face-attribute 'default :background)))
 ;;   (custom-set-faces
-;;    `(company-tooltip ((t (:inherit default :background ,(color-darken-name bg 10)))))
-;;    `(company-preview ((t (:inherit default :background ,(color-darken-name bg 20)))))
-;;    `(company-preview-common ((t (:inherit default :background ,(color-lighten-name bg 20)))))
+;;    `(company-tooltip ((t
+;; (:inherit default :background ,(color-darken-name bg 10)))))
+;;    `(company-preview ((t
+;;; (:inherit default :background ,(color-darken-name bg 20)))))
+
+;;    `(company-preview-common ((t
+;; (:inherit default :background ,(color-lighten-name bg 20)))))
 ;;    `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 20)))))
 ;;    `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
 ;;    `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
