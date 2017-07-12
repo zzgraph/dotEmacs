9230ae99f5f72c1805ef5630e82186f2b839dcde
Shahab Shahsabvari Alavidjeh <shahab.shahsavari@outlook.com>
2017-07-12 16:22:02 +0430

    Revert "Revert "Use json mode for .tern-project files""
    This reverts commit 91dbbe5b7f7d5ec1762fdcae9daa4dfc81685cfc.

--- misc/jsInit.el
+++ misc/jsInit.el
@@ -8,6 +8,7 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(add-to-list 'auto-mode-alist '(".tern-project" . json-mode))
 
 (setq js-basic-indent 2)
 (setq-default js2-basic-indent 2
