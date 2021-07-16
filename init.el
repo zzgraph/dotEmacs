288784cb2d7ebc7c8011951a8cfa6f56698ba259
Shahab Shahsavari Alavidjeh <zzgraph@gmail.com>
2021-07-16 12:24:08 +0430

    adding secrets.el to startup to better integrate with gnome keyring

--- init.el
+++ init.el
@@ -2,16 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;; (package-initialize)
 
-;;; (require 'tls)
-;; (require 'epa-file)
-;; (epa-file-enable)
-
+(require secrets)
 
 (defvar misc-settings-folder)
 (setq misc-settings-folder
