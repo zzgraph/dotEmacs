1448f09a0cb0998a9dcb7ebb60f706cffb4cd266
Shahab Shahsavari Alavidjeh <zzgraph@gmail.com>
2021-07-18 09:11:17 +0430

    Try to add word count to modeline with wc mode

--- misc/modeLineInit.el
+++ misc/modeLineInit.el
@@ -3,8 +3,6 @@
 ;; try to customize modeline with all-the-icons
 
 ;;; Code:
-(require 'all-the-icons)
-;; (require 'spaceline-config)
 
 ;; display time in modeline
 (display-time-mode 1)
@@ -12,49 +10,31 @@
 (size-indication-mode t)
 (display-battery-mode t)
 (column-number-mode t)
-;; (add-hook 'after-init-hook #'fancy-battery-mode)
+
+;; Add word count to modeline
+
+(setq mode-line-position
+      (append
+       mode-line-position
+       '((wc-mode
+	  (6 (:eval (if (use-region-p)
+			(format " %d,%d,%d"
+				(abs (- (point) (mark)))
+				(count-words-region (point) (mark))
+				(abs (- (line-number-at-pos (point))
+					(line-number-at-pos (mark)))))
+		      (format " %d,%d,%d"
+			      (- (point-max) (point-min))
+			      (count-words-region (point-min) (point-max))
+			      (line-number-at-pos (point-max))))))
+	  nil))))
 
 
-;; (spaceline-all-the-icons-theme)
 
 ;; Modeline (smart-mode-line)
+(require 'smart-mode-line)
 (sml/setup)
 (setq sml/theme 'respectful)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; the mode line format as it set by smart-mode-line                          ;;
-;;                                                                            ;;
-;; ("%e" mode-line-front-space mode-line-mule-info mode-line-client           ;;
-;; mode-line-modified mode-line-remote mode-line-frame-identification         ;;
-;; mode-line-buffer-identification sml/pos-id-separator mode-line-position    ;;
-;; (vc-mode vc-mode)                                                          ;;
-;; ml/pre-modes-separator mode-line-modes mode-line-misc-info                 ;;
-;; mode-line-end-spaces)                                                      ;;
-;; )                                                                          ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-
-;; Let's have some mode-line customization fun
-
-;; (set-face-attribute 'mode-line nil
-;; 		    :height 2)
-
-;; (set-face-attribute 'mode-line-inactive nil
-;;                     :box '(:width 0)
-;; 		    :box '(:line-width 0)
-;; 		    :box '(:color "white")
-;; 		    :box '(:style nil)
-;; 		    :height 1.1
-;; 		    :foreground "inherit"
-;; 		    :background "inherit")
-
-
-;; (require 'powerline) (powerline-default-theme) (setq
-;; powerline-default-separator 'arrow) ;; (load-theme
-;; 'airline-solarized-gui t) (load-theme 'airline-hybridline t)
-
 
 (provide 'modeLineInit)
 ;;; modeLineInit.el ends here
